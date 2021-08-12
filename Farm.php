99ce464eeed09a78863cf8888833349608bf9c5e
Your Name <you@example.com>
2021-08-12 17:57:11 +0300

    Farm class completed

--- Farm.php
+++ Farm.php
@@ -3,5 +3,32 @@
 
 class Farm
 {
+    private $animal_list;
+    private $cart;
+    private $counter = 0;
 
+    public function registerAnimal(Animal $animal)
+    {
+        $this->animal_list[] = $animal;
+        $animal->setId($this->counter);
+        $this->counter++;
+    }
+
+    public function collectAll()
+    {
+        foreach ($this->animal_list as $animal) {
+            $this->cart[] = $animal->collect();
+        }
+        return $this->getCart();
+    }
+
+    public function getCart()
+    {
+        foreach ($this->cart as $item) {
+            $array[$item['type']] = [
+                'count' => $array[$item['type']]['count'] + $item['count'],
+                'ending' => $item['ending']];
+        }
+        return $array;
+    }
 }
