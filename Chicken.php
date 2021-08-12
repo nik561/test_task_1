98177306c5bd87fe28bfaf5164771c2aac89268a
Your Name <you@example.com>
2021-08-12 17:56:48 +0300

    Cow and Chicken classes completed

--- Chicken.php
+++ Chicken.php
@@ -3,5 +3,12 @@
 
 class Chicken extends Animal
 {
+    protected $count_for_once;
+    protected $ending = '';
+    protected $collect_type = 'egg';
 
+    function __construct()
+    {
+        $this->count_for_once = rand(0, 1);
+    }
 }
--- Cow.php
+++ Cow.php
@@ -3,5 +3,12 @@
 
 class Cow extends Animal
 {
+    protected $count_for_once;
+    protected $ending = 'liters';
+    protected $collect_type = 'milk';
 
+    function __construct()
+    {
+        $this->count_for_once = rand(8, 12);
+    }
 }
