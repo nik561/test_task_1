882429b28c0aa2ad682516768ff2b519e694fea8
Your Name <you@example.com>
2021-08-12 17:57:35 +0300

    Main.php completed

--- main.php
+++ main.php
@@ -2,5 +2,11 @@
 spl_autoload_register(function ($class) {
     include $class . '.php';
 });
-
+$factory = new AnimalFactory();
+$farm = new Farm();
+$factory->createAnimal($farm, 'Chicken', 15);
+$factory->createAnimal($farm, 'Cow', 7);
+$cart = $farm->collectAll();
+foreach ($cart as $key => $item) {
+    echo $key . ': ' . $item['count'] . ' ' . $item['ending'] . PHP_EOL;
 }
