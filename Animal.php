45c8f6c98f94d6b1a3407ab19d8157648854a8f1
Your Name <you@example.com>
2021-08-12 17:56:19 +0300

    Animal class completed

--- Animal.php
+++ Animal.php
@@ -3,5 +3,23 @@
 
 abstract class Animal
 {
+    protected $id = NULL;
+    protected $count_for_once = 0;
+    protected $collect_time;
+    protected $ending;
+    protected $collect_type;
 
+    public final function setId(int $id)
+    {
+        if (!is_null($this->id)) {
+            throw new Exception('You can\'t reset ID');
+        }
+        $this->id = $id;
+    }
+
+    public function collect()
+    {
+        $this->collect_time = time();
+        return array('type' => $this->collect_type, 'count' => $this->count_for_once, 'animal_id' => $this->id, 'ending' => $this->ending);
+    }
 }
